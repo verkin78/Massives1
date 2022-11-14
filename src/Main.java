0d86a8c58921e32ffe11b57e74441cbe8cfd1d01
Verkin78 <otrajenie.jc@gmail.com>
2022-11-14 19:26:40 +0300

    Задача3

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,13 @@ public class Main {
         int n1 = number [0];
         int n2 = number [1];
         int n3 = number [2];
-        System.out.println(n1 + "," + n2 + "," + n3);
+        System.out.println(n3 + "," + n2 + "," + n1);
 
         double[] Number = {1.57, 7.654, 9.986};
         double N1 = Number [0];
         double N2 = Number [1];
         double N3 = Number [2];
-        System.out.println(N1 + "," + N2 + "," + N3);
+        System.out.println(N3 + "," + N2 + "," + N1);
 
         int[] numbers = new int[]{5, 10, 15, 20, 25};
         int ns1 = numbers [0];
@@ -18,7 +18,7 @@ public class Main {
         int ns3 = numbers [2];
         int ns4 = numbers [3];
         int ns5 = numbers [4];
-        System.out.println(ns1 + "," + ns2 + "," + ns3 + "," + ns4 + "," + ns5);
+        System.out.println(ns5 + "," + ns4 + "," + ns3 + "," + ns2 + "," + ns1);
 
 
     }
