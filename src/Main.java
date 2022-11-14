709528c1972b8a44793d63bf78be0ab9db6de1a3
Verkin78 <otrajenie.jc@gmail.com>
2022-11-14 18:46:51 +0300

    Задача1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         }
+        int [] number = new int [] {1,2,3};
+    double [] Number = {1.57, 7.654, 9.986};
+    int [] numbers = new int [] {5,10,15,20,25};
+
+
 }
