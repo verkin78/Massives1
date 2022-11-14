72e747585dc7dfae66a39bb3e68807e882dce3c4
Verkin78 <otrajenie.jc@gmail.com>
2022-11-14 19:47:06 +0300

    Задача4

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         int[] number = new int[]{1, 2, 3};
-        int n1 = number [0];
-        int n2 = number [1];
-        int n3 = number [2];
-        System.out.println(n3 + "," + n2 + "," + n1);
-
-        double[] Number = {1.57, 7.654, 9.986};
-        double N1 = Number [0];
-        double N2 = Number [1];
-        double N3 = Number [2];
-        System.out.println(N3 + "," + N2 + "," + N1);
-
-        int[] numbers = new int[]{5, 10, 15, 20, 25};
-        int ns1 = numbers [0];
-        int ns2 = numbers [1];
-        int ns3 = numbers [2];
-        int ns4 = numbers [3];
-        int ns5 = numbers [4];
-        System.out.println(ns5 + "," + ns4 + "," + ns3 + "," + ns2 + "," + ns1);
-
-
+        number[0] += 1;
+        number[2] += 1;
+        for (int i = 0; i < number.length; i++) {
+            System.out.print(number[i] + " ");
+        }
     }
-
-
 
 
 }
